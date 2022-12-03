4346d113f9ff3242a6eea8503ebd720d04da2fe4
Marco Zanino <marco.zanino@gmail.com>
2022-12-03 18:31:27 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.7.22"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest()
+        }
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("com.zanyx.advent.AppKt")
+}
